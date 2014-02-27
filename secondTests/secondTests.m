3b9aa1c4a941df20c85512956726f47206f2ddf3
iefmac002 <iefmac002@iefmac002tekiiMac.local>
2014-02-27 15:14:28 +0800

    Initial Commit

---
+++ secondTests/secondTests.m
@@ -0,0 +1,34 @@
+//
+//  secondTests.m
+//  secondTests
+//
+//  Created by iefmac002 on 14-2-27.
+//  Copyright (c) 2014年 iefmac002. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface secondTests : XCTestCase
+
+@end
+
+@implementation secondTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
